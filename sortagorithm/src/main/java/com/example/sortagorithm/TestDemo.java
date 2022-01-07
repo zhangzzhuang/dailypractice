b051646b2d00941402e700aefc457a9938e126c9
zhangzz <992526731@qq.com>
2022-01-07 11:29:36 +0800

    modify a

--- sortagorithm/src/main/java/com/example/sortagorithm/TestDemo.java
+++ sortagorithm/src/main/java/com/example/sortagorithm/TestDemo.java
@@ -15,84 +15,82 @@ public class TestDemo implements ApplicationRunner {
 
 	public static void main(String[] args) {
 
-//
-//		Thread a = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				System.out.println("a");
-//				try {
-//					Thread.sleep(5000);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//				System.out.println("a5000");
-//			}
-//		}, "a");
-//
-//		Thread b = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				try {
-//					a.join();
-//					System.out.println("b");
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}, "b");
-//
-//		Thread c = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				try {
-//					b.join();
-//					System.out.println("c");
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}, "c");
-//
-//		b.start();
-//		c.start();
-//		a.start();
-//
-////
-////		String s = "basi";
-////		int hash = s.hashCode();
-////		int hash16 = hash >>> 16;
-////		int key = hash ^ hash16;
-////
-////		System.out.println(hash);
-////		System.out.println(hash16);
-////		System.out.println(key);
-////
-////		System.out.println(hash(s));
-////
-////
-////		System.out.println((16-1) & hash);
-////
-//		int money = 1000;
-//		int num = 10;
-//
-//		System.out.println(redPacket(900, 10));
 
-		Map<String, String> map = new HashMap<>();
-		map.put(null,null);
-		map.put(null,"null;");
-		System.out.println(map.size());
-		System.out.println(map.get(null));
-		LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
-		linkedHashMap.put(null,null);
-		System.out.println(linkedHashMap.size());
+		Thread a = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("a");
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("a5000");
+			}
+		}, "a");
 
+		Thread b = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					a.join();
+					System.out.println("b");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "b");
+
+		Thread c = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					b.join();
+					System.out.println("c");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "c");
+
+		b.start();
+		c.start();
+		a.start();
+
+//
+//		String s = "basi";
+//		int hash = s.hashCode();
+//		int hash16 = hash >>> 16;
+//		int key = hash ^ hash16;
+//
+//		System.out.println(hash);
+//		System.out.println(hash16);
+//		System.out.println(key);
+//
+//		System.out.println(hash(s));
+//
+//
+//		System.out.println((16-1) & hash);
+//
+		int money = 1000;
+		int num = 10;
+
+		System.out.println(redPacket(900, 10));
+
+//		Map<String, String> map = new HashMap<>();
+//		map.put(null,null);
+//		map.put(null,"null;");
+//		System.out.println(map.size());
+//		System.out.println(map.get(null));
+//		LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+//		linkedHashMap.put(null,null);
+//		System.out.println(linkedHashMap.size());
+//
 //		TreeMap treeMap = new TreeMap<>();
 //		treeMap.put(null,null);
 //		System.out.println(treeMap);
 
 
-		Hashtable hashtable = new Hashtable();
-		hashtable.put(null,null);
 	}
 
 
